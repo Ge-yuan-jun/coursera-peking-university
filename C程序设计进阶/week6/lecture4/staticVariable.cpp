d3428479b982148eb12de980bc78ae9a07780990
GeYuanjun <daydaygyj@gmail.com>
2016-06-23 17:20:34 +0800

    add example

--- C程序设计进阶/week6/lecture4/staticVariable.cpp
+++ C程序设计进阶/week6/lecture4/staticVariable.cpp
@@ -3,7 +3,7 @@
 * @Date:   2016-06-23 17:05:58
 * @Email:  geyuanjun.sh@superjia.com
 * @Last modified by:   geyuanjun
-* @Last modified time: 2016-06-23 17:07:44
+* @Last modified time: 2016-06-23 17:09:48
 */
 
 
@@ -11,3 +11,6 @@
 //静态局部变量
 //  函数中的局部变量的值在函数调用结束后不消失而保留原值
 //      即其占用的存储单元不释放，在下一次改函数调用时，仍可以继续使用该变量
+//  用关键字static进行声明，可将变量指定为“静态局部变量”
+//      static int value1 = 20;
+ 
