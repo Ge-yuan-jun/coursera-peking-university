9a12c782410aa5b4165897b6ceeaacc3e1160c6f
GeYuanjun <daydaygyj@gmail.com>
2016-06-23 17:08:10 +0800

    the definition

---
+++ C程序设计进阶/week6/lecture4/staticVariable.cpp
@@ -0,0 +1,13 @@
+/**
+* @Author: geyuanjun
+* @Date:   2016-06-23 17:05:58
+* @Email:  geyuanjun.sh@superjia.com
+* @Last modified by:   geyuanjun
+* @Last modified time: 2016-06-23 17:07:44
+*/
+
+
+
+//静态局部变量
+//  函数中的局部变量的值在函数调用结束后不消失而保留原值
+//      即其占用的存储单元不释放，在下一次改函数调用时，仍可以继续使用该变量
