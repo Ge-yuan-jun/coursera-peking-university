11d20cce84dbdd31bf357976ea59f83b4cff2767
GeYuanjun <daydaygyj@gmail.com>
2016-07-08 15:45:56 +0800

    add question description

--- C程序设计进阶/week6/homework/test2.cpp
+++ C程序设计进阶/week6/homework/test2.cpp
@@ -1,3 +1,26 @@
+/**
+* @Author: geyuanjun
+* @Date:   2016-07-08 14:49:10
+* @Email:  geyuanjun.sh@superjia.com
+* @Last modified by:   geyuanjun
+* @Last modified time: 2016-07-08 15:45:28
+*/
+
+/*
+描述
+
+脱氧核糖核酸（DNA）由两条互补的碱基链以双螺旋的方式结合而成。而构成DNA的碱基共有4种，分别为腺瞟呤（A）、鸟嘌呤（G）、胸腺嘧啶（T）和胞嘧啶（C）。我们知道，在两条互补碱基链的对应位置上，腺瞟呤总是和胸腺嘧啶配对，鸟嘌呤总是和胞嘧啶配对。你的任务就是根据一条单链上的碱基序列，给出对应的互补链上的碱基序列。
+
+输入
+
+第一行是一个正整数n，表明共有n条要求解的碱基链。
+
+以下共有n行，每行用一个字符串表示一条碱基链。这个字符串只含有大写字母A、T、G、C，分别表示腺瞟呤、胸腺嘧啶、鸟嘌呤和胞嘧啶。每条碱基链的长度都不超过255。
+
+输出
+
+共有n行，每行为一个只含有大写字母A、T、G、C的字符串。分别为与输入的各碱基链互补的碱基链。
+ */
 #include<iostream>
 using namespace std;
 int main(){
@@ -6,10 +29,10 @@ int main(){
 	cin.get();
 	for (int i = 0; i < n; i++) {
 		char a[256];
-		cin.getline(a,256);// ���������ж�ȡһ���ַ�
+		cin.getline(a,256);// ���������ж�ȡһ���ַ�
 		char newA[256];
 		for (int j = 0; j < 256; j++) {
-			// ֻ��ͨ����ȡһ�еķ�ʽ���������룬����һ������һ�ߴ��� 
+			// ֻ��ͨ����ȡһ�еķ�ʽ���������룬����һ������һ�ߴ���
 			if (a[j] == 'A') {
 				newA[j] = 'T';
 			} else if (a[j] == 'T') {
@@ -23,14 +46,14 @@ int main(){
 				break;
 			}
 		}
-		
+
 		for (int j = 0; j < 256; j++){
 			if (newA[j] != '\0') {
 				cout << newA[j];
 			} else {
 				cout << endl;
-				break;// �������������ѭ�� 
-			}	
+				break;// ����������������ѭ��
+			}
 		}
 	}
 	return 0;
